d5653065817c92e7d2bd49b4e09ff47232407d98
Nathan Lindekens <nathan.lindekens@outlook.com>
2019-02-22 11:17:15 +0100

    first commit

---
+++ _BACK_/_SITE_/server.js
@@ -0,0 +1,11 @@
+const X = require('express');
+const APP = X();
+const PORT =1403;
+
+
+
+APP.use('/', X.static(__dirname + '/_SITE_/'));
+
+APP.listen(PORT, () => {
+      console.log(`\r\nNODE ::: I started my back end server on port ${PORT}.\r\n`);
+  });
